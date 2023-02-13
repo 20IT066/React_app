763c4307f5cdff8a49cf689f0df84f7454e24aee
Parth Mangukiya <88765995+20IT066@users.noreply.github.com>
2023-02-13 21:09:07 +0530

    update for check

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ export default function Footer() {
       <Navbar fixed="bottom" bg="dark" variant="dark">  
         <Container  className="text-center text-muted">
             <Col lg={12} className="text-center text-light">
-            {year} - All Rights are Reserved!!! by parth
+            {year} - All Rights are Reserved!!! by mangukiya
             </Col>
         </Container>
       </Navbar>
